27c3712e28a29f6aa1d375263a338d8e90b028c4
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-08-10 13:19:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/api/test", (req, res) => {
+    res.send({ response: "Shawty, your testing is successful" });
+});
+
+const hostname = process.env.HOSTNAME || "127.0.0.1";
+const port = process.env.PORT || 3000;
+app.listen(port, hostname, () => {
+    console.log("Server ascolta sulla porta: " + 3000);
+    console.log(`URL: http://${hostname}:${port}/`);
+});
